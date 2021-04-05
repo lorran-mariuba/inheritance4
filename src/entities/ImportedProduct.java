4faa574c6d157439b5451b194eb9fdfca1b757b6
Lorran Mariuba de Oliveira <llorrann@gmail.com>
2021-04-04 20:02:15 -0400

    Exercise about inheritance with Override.

---
+++ src/entities/ImportedProduct.java
@@ -0,0 +1,36 @@
+package entities;
+
+public class ImportedProduct extends Product {
+	
+	private Double customsFree;
+	
+	public ImportedProduct() {
+		super();
+	}
+
+	public ImportedProduct(String name, Double price, Double customsFree) {
+		super(name, price);
+		this.customsFree = customsFree;
+	}
+
+	public ImportedProduct(Double customsFree) {
+		this.customsFree = customsFree;
+	}
+
+	public Double getCustomsFree() {
+		return customsFree;
+	}
+
+	public void setCustomsFree(Double customsFree) {
+		this.customsFree = customsFree;
+	}
+	
+	public double totalPrice() {
+		return price + customsFree;
+	}
+	
+	@Override
+	public String priceTag() {
+		return name + " $ " + String.format("%.2f (Costumes free: $ %.2f)", totalPrice(), price);
+	}
+}
